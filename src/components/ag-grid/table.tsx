8051868458875d3cdb3b4547feda374a2a835018
kanghee-adeeple <lkh75@adeeple.com>
2025-06-05 17:29:19 +0900

    AG Grid 컴포넌트 주석으로 안내내

--- src/components/ag-grid/table.tsx
+++ src/components/ag-grid/table.tsx
@@ -1,24 +1,14 @@
 "use client";
 
-import React, {
-  //   useCallback,
-  useMemo,
-  //   useRef,
-  useState,
-  //   StrictMode,
-} from "react";
-// import { createRoot } from "react-dom/client";
-import { AgGridReact } from "ag-grid-react";
+import React, { useMemo, useState } from "react"; // 리액트 훅
+import { AgGridReact } from "ag-grid-react"; // AG Grid의 테이블 컴포넌트
 import {
   ClientSideRowModelModule,
   ColDef,
-  //   ColGroupDef,
-  //   GridApi,
-  //   GridOptions,
   ModuleRegistry,
-  //   SideBarDef,
   ValidationModule,
-} from "ag-grid-community";
+} from "ag-grid-community"; // AG Grid의 무료 라이선스 중 필요한 모듈 불러오기
+
 import {
   ColumnMenuModule,
   ColumnsToolPanelModule,
@@ -26,7 +16,8 @@ import {
   FiltersToolPanelModule,
   PivotModule,
   RowGroupingPanelModule,
-} from "ag-grid-enterprise";
+} from "ag-grid-enterprise"; // AG Grid의 유료 라이선스 중 필요한 모듈 불러오기
+
 ModuleRegistry.registerModules([
   ClientSideRowModelModule,
   ColumnsToolPanelModule,
@@ -36,8 +27,9 @@ ModuleRegistry.registerModules([
   FiltersToolPanelModule,
   RowGroupingPanelModule,
   ...(process.env.NODE_ENV !== "production" ? [ValidationModule] : []),
-]);
-import { useFetchJson } from "../../api/useFetchJson";
+]); // AG Grid의 모듈 사용하기
+
+import { useFetchJson } from "../../api/useFetchJson"; // 임시 데이터 불러오는 파일 불러오기
 
 export interface IOlympicData {
   athlete: string;
@@ -50,18 +42,19 @@ export interface IOlympicData {
   silver: number;
   bronze: number;
   total: number;
-}
+} // TypeScript의 파일 형식 지정하기
 
 const GridExample = () => {
-  const containerStyle = useMemo(() => ({ width: "100%", height: "100%" }), []);
-  const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
+  const containerStyle = useMemo(() => ({ width: "100%", height: "100%" }), []); // 스타일에 대한 내용 (크게 상관 없음)
+  const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []); // 스타일에 대한 내용 (크게 상관 없음)
 
   const [columnDefs, setColumnDefs] = useState<ColDef[]>([
     { field: "country", rowGroup: true },
     { field: "sport", pivot: true },
     { field: "year", pivot: true },
     { field: "gold", aggFunc: "sum" },
-  ]);
+  ]); // 테이블 헤더 지정하기
+
   const defaultColDef = useMemo<ColDef>(() => {
     return {
       flex: 1,
@@ -70,30 +63,31 @@ const GridExample = () => {
       enableRowGroup: true,
       enablePivot: true,
     };
-  }, []);
+  }, []); // 테이블 헤더 기본 값 지정
+
   const autoGroupColumnDef = useMemo<ColDef>(() => {
     return {
       minWidth: 200,
       pinned: "left",
     };
-  }, []);
+  }, []); // 그룹지어진 칼럼 속성 지정
 
   const { data, loading } = useFetchJson<IOlympicData>(
     "https://www.ag-grid.com/example-assets/olympic-winners.json"
-  );
+  ); // 데이터 및 로딩 불러오기
 
   return (
     <div style={containerStyle}>
       <div style={gridStyle}>
         <AgGridReact<IOlympicData>
-          rowData={data}
-          loading={loading}
-          columnDefs={columnDefs}
-          defaultColDef={defaultColDef}
-          autoGroupColumnDef={autoGroupColumnDef}
-          pivotMode={true}
-          sideBar={"columns"}
-          pivotPanelShow={"always"}
+          rowData={data} // 테이블에 실제 그려줄 데이터
+          loading={loading} // 로딩
+          columnDefs={columnDefs} // 테이블 헤더
+          defaultColDef={defaultColDef} // 칼럼에 대한 기본 속성
+          autoGroupColumnDef={autoGroupColumnDef} // 같은 값에 대해
+          pivotMode={true} // 피봇테이블 사용 여부
+          sideBar={"columns"} // 사이드바 제목
+          pivotPanelShow={"always"} // 피봇패널 노출 여부
         />
       </div>
     </div>
